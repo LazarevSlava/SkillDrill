dcd665aef627acc71402f0522c5a5e2895a0cbf1
LazarevSlava <549132w@gmail.com>
2025-05-07 12:06:17 +0300

    ✅ fix: ESLint config now respects JS and TS separation

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,36 @@ import tseslint from 'typescript-eslint';
 
 export default [
     js.configs.recommended,
-    ...tseslint.configs.recommended,
+
     {
         ignores: ['**/node_modules/**', '**/dist/**'],
-        files: ['client/**/*.{js,ts,tsx}', 'server/**/*.{js,ts}'],
+
+        files: ['client/**/*.{ts,tsx}'],
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
             parser: tseslint.parser,
             parserOptions: {
-                project: [
-                    './client/tsconfig.app.json',
-                ],
+                project: './client/tsconfig.app.json',
             },
             globals: {
                 ...globals.browser,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+        },
+        rules: {
+            // твои правила
+        },
+    },
+
+    {
+        files: ['**/*.{js,cjs,mjs}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
                 ...globals.node,
             },
         },
