490ccc05cd91b9f6816128c124213f5d325b833c
LazarevSlava <549132w@gmail.com>
2025-05-07 12:09:30 +0300

    💄 style: format with Prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,35 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-    const handleServer = () => {
-        fetch('http://localhost:3001/api/hello', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name: 'Slava' }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data.message);
-            });
-        console.log('🧪 testing GitHub Actions');
-    };
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
-            <div className="bg-white dark:bg-gray-900 shadow-md rounded-xl p-8 text-center">
-                <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
-                    Hello Tailwind!
-                </h1>
-                <button
-                    onClick={handleServer}
-                    className="px-6 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition"
-                >
-                    Click me
-                </button>
-            </div>
-        </div>
-    );
+  const handleServer = () => {
+    fetch("http://localhost:3001/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "Slava" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message);
+      });
+    console.log("🧪 testing GitHub Actions");
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
+      <div className="bg-white dark:bg-gray-900 shadow-md rounded-xl p-8 text-center">
+        <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
+          Hello Tailwind!
+        </h1>
+        <button
+          onClick={handleServer}
+          className="px-6 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition"
+        >
+          Click me
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,42 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
-    js.configs.recommended,
+  js.configs.recommended,
 
-    {
-        ignores: ['**/node_modules/**', '**/dist/**'],
+  {
+    ignores: ["**/node_modules/**", "**/dist/**"],
 
-        files: ['client/**/*.{ts,tsx}'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            parser: tseslint.parser,
-            parserOptions: {
-                project: './client/tsconfig.app.json',
-            },
-            globals: {
-                ...globals.browser,
-            },
-        },
-        plugins: {
-            '@typescript-eslint': tseslint.plugin,
-        },
-        rules: {
-            // твои правила
-        },
+    files: ["client/**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./client/tsconfig.app.json",
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
-
-    {
-        files: ['**/*.{js,cjs,mjs}'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            globals: {
-                ...globals.node,
-            },
-        },
-        rules: {},
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
     },
+    rules: {
+      // твои правила
+    },
+  },
+
+  {
+    files: ["**/*.{js,cjs,mjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {},
+  },
 ];
